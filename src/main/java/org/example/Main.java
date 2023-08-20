e06ac79eaa1c1ee1a790b42a21fa153a2f8dc0ea
R.K.Suleimanov <crangoren@gmail.com>
2023-08-20 18:54:57 +0300

    lesson 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,15 +14,26 @@ public class Main {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\R.K.Suleimanov\\IdeaProjects\\chromedriver\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
-        driver.get("https://google.com");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.get("https://google.com");
+//        driver.get("https://avito.ru");
+        driver.get("https://avito.ru/all/avtomobili");
 
-        WebElement element = new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.id("123")));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
 
-        WebElement input = driver.findElement(By.xpath("//textarea[@jsname='yZiJbe']"));
-        
-        input.click();
+
+
+//        WebElement element = driver.findElement(By.xpath("//a[text()='Недвижимость']"));
+
+//        String isTranslate = element.getAttribute("translate");
+//        String href = element.getAttribute("href");
+//        System.out.println(href);
+//        System.out.println(isTranslate);
+//        WebElement element = new WebDriverWait(driver, Duration.ofSeconds(10))
+//                .until(ExpectedConditions.presenceOfElementLocated(By.id("123")));
+
+//        WebElement input = driver.findElement(By.xpath("//textarea[@jsname='yZiJbe']"));
+//
+//        input.click();
 
     }
 }
